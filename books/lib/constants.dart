b6b1f98e645efac890b40ec371c00c5695c60b06
Etroynov <troinof@yandex.ru>
2020-09-23 01:02:50 +0300

    feat(book): add splash screen

---
+++ books/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF393939);
+const secondaryColor = Color(0xFF8F8F8F);
+const iconColor = Color(0xFFF48A37);
+const progressIndicatorColor = Color(0xFFBE7066);
--- books/lib/main.dart
+++ books/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:books/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'package:books/constants.dart';
+import 'package:books/widgets/rounded_button.dart';
 
 void main() {
   runApp(App());
@@ -8,8 +12,59 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Container(
-      child: Text('Test'),
-    ));
+      debugShowCheckedModeBanner: false,
+      title: 'Book App',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          textTheme:
+              Theme.of(context).textTheme.apply(displayColor: primaryColor)),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg.png"), fit: BoxFit.fill)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                  style: Theme.of(context).textTheme.headline2,
+                  children: [
+                    TextSpan(text: "Flamin"),
+                    TextSpan(
+                        text: "go.",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ]),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              child: RoundedButton(
+                text: "start reading",
+                fontSize: 20,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return HomeScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
