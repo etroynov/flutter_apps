e1b2b9a9e71acfc1a41615f5e87d9875d2c1397b
Etroynov <troinof@yandex.ru>
2020-09-29 15:06:20 +0300

    feat(book): complite popular day card

--- books/lib/screens/home_screen.dart
+++ books/lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:books/constants.dart';
+import 'package:books/widgets/book_rating.dart';
 import 'package:books/widgets/reading_list_card.dart';
+import 'package:books/widgets/two_side_rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -55,12 +58,6 @@ class HomeScreen extends StatelessWidget {
                     image: "assets/images/book-2.png",
                     rating: 4.8,
                   ),
-                  ReadingListCard(
-                    title: "Crushing & Influence",
-                    author: "Gary Venchunk",
-                    image: "assets/images/book-3.png",
-                    rating: 4.9,
-                  ),
                   SizedBox(
                     width: 30,
                   )
@@ -84,30 +81,114 @@ class HomeScreen extends StatelessWidget {
                           )
                         ]),
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 20),
-                    width: double.infinity,
-                    height: 205,
-                    child: Stack(
-                      children: <Widget>[
-                        Positioned(
-                          child: Container(
-                            height: 185,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Color(0xFFEAEAEA).withOpacity(.45),
-                              borderRadius: BorderRadius.circular(29),
-                            ),
-                          ),
-                        )
+                  bestOfTheDayCard(size, context),
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.headline4,
+                      children: [
+                        TextSpan(text: "Contine"),
+                        TextSpan(text: "reading..."),
                       ],
                     ),
-                  )
+                  ),
                 ],
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Container bestOfTheDayCard(Size size, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      width: double.infinity,
+      height: 205,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 24,
+                top: 24,
+                right: size.width * .35,
+              ),
+              height: 185,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFEAEAEA).withOpacity(.45),
+                borderRadius: BorderRadius.circular(29),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "New York Time Best For 11th March 2020",
+                    style: TextStyle(
+                      fontSize: 9,
+                      color: secondaryColor,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "How To Win\nFriends & Influence",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Text(
+                    "Gary Venchunk",
+                    style: TextStyle(
+                      color: secondaryColor,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      BookRating(
+                        score: 4.9,
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          "When the earth was flat and everyone wanted to win game of the best and people...",
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: secondaryColor,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Image.asset(
+              "assets/images/book-3.png",
+              width: size.width * .37,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SizedBox(
+              height: 40,
+              width: size.width * .3,
+              child: TwoSideRoundedButton(
+                text: "Read",
+                radious: 24,
+                press: () {},
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
